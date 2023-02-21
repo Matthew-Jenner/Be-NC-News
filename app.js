88a4af26ecd13c0189ab0abb3df99d06b897a99a
Matthew Jenner <matthewjenner@hotmail.com>
2023-02-21 10:36:46 +0000

    refactored test task 1

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const app = express()
 const { getTopics } = require("./controlllers/ncNewsController")
-const {handle404Statuses, handle500Statuses} = require("./controlllers/errorHandlingController")
-app.use(express.json())
+const {handle500Statuses} = require("./controlllers/errorHandlingController")
+//app.use(express.json()) 
 
 app.get("/api/topics", getTopics)
 
 
 app.all('*', (req, res, next)=>{
-    next({status:404})
+    res.status(404).send({message: "invalid pathway"})
 })
 
-app.use(handle404Statuses);
+
 app.use(handle500Statuses)
 
 module.exports = app;
