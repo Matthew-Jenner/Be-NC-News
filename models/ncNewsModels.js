58f286de6e6fa390afbb5a8a6f13b543d6f6fa87
Matthew Jenner <matthewjenner@hotmail.com>
2023-02-22 10:37:49 +0000

    GET all comments with tests completed

--- models/ncNewsModels.js
+++ models/ncNewsModels.js
@@ -29,3 +29,16 @@ exports.fetchArticles = () => {
             }
         })
     }
+    exports.fetchComments = (article_id) => {
+        return db.query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY comments.created_at DESC`, [article_id])
+        .then((result) => {
+            if(result.rows.length===0){
+                return Promise.reject({
+                    status: 404,
+                    message: "article id not found"
+                }) 
+            }else{
+                return result.rows
+            }
+        })
+    }
